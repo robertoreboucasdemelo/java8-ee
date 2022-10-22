a163f051b9af8202d12f128c265b1fc2ae5e4b91
Roberto Reboucas de Melo <roberto.reboucas@ibm.com>
2022-10-22 18:04:00 -0300

    Integrating JSON-P

--- src/main/java/com/airhacks/boundary/CarsResource.java
+++ src/main/java/com/airhacks/boundary/CarsResource.java
@@ -4,6 +4,10 @@ package com.airhacks.boundary;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.stream.JsonCollectors;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -13,6 +17,8 @@ import javax.ws.rs.core.MediaType;
 
 import com.airhacks.entity.Car;
 import com.airhacks.entity.Specification;
+import com.airhacks.enums.Color;
+import com.airhacks.enums.EngineType;
 
 @Path("cars")
 @Produces(MediaType.APPLICATION_JSON)
@@ -23,12 +29,22 @@ public class CarsResource {
 	CarManufacturer carManufacturer;
 	
 	@GET
-	public List<Car> retrieveCars(){
-		return carManufacturer.retrieveCars();
+	public JsonArray retrieveCars(){
+		return carManufacturer.retrieveCars()
+				.stream()
+				.map(c -> Json.createObjectBuilder()
+						.add("color", c.getColor().name())
+						.add("engine", c.getEngineType().name())
+						.add("id", c.getIdentifier())
+						.add("hello", "value")
+						.build())
+				.collect(JsonCollectors.toJsonArray());
 	}
 	
 	@POST
-	public void createCar(Specification specification) {
-		carManufacturer.manufactureCar(specification);
+	public void createCar(JsonObject jsonObject) {
+		Color color = Color.valueOf(jsonObject.getString("color"));
+		EngineType engine= EngineType.valueOf(jsonObject.getString("engine"));
+		carManufacturer.manufactureCar(new Specification(color, engine));
 	}
 }
